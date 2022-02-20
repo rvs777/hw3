754af903c447712d6407639d0cf237c2ad356d57
VenkataSudhakar(VESU) Rayaprolu <vesu.rayaprolu@kellogg.northwestern.edu>
2022-02-20 03:05:35 +0000

    WIP-3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,13 @@ class PostsController < ApplicationController
     
     def new
         @post = Post.new
+        @post.place_id = params["place_id"]
     end
 
     def create
         @post = Post.new(params["post"])
         @post.save
-        redirect_to "places/posts"
+        redirect_to "/places/#{@post.place_id}"
       end
    
 end
